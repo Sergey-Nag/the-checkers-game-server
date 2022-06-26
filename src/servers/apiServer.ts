c8ab50191aad80f8f94b378828c00f868fc607df
Serhii Nahornyi <snah@ciklum.com>
2022-06-26 09:05:57 +0300

    root page

--- src/servers/apiServer.ts
+++ src/servers/apiServer.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import Room from '../models/Room';
+import { join } from 'path';
 
 const app = express();
 
@@ -9,6 +10,10 @@ app.use(cors());
 const run = (port: number) => {
   app.listen(port, () => console.log(`Api server launched on port: ${port}`));
 };
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, '../../', 'public', 'index.html'));
+});
 
 app.get('/api/rooms', (req, res) => {
   const rooms = Room.rooms;
