b9c83344b5d11ec2c1c2281f1cda5ce86822b60d
Serhii Nahornyi <snah@ciklum.com>
2022-07-03 07:20:23 +0300

    swagger host

--- src/servers/apiDocs/apiDocs.ts
+++ src/servers/apiDocs/apiDocs.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express';
 import { serve, setup } from 'swagger-ui-express';
 import apiDocs from './swagger.json';
+console.log(apiDocs);
+
+apiDocs.host = process.env.HOST ?? 'localhost:8080';
 
 const apiDocRouter = Router();
 
